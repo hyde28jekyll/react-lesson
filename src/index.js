cd47e51e0738e9667af8ecff0a9a6857f6ab0da3
hyde28jekyll <86185882+hyde28jekyll@users.noreply.github.com>
2023-01-02 15:04:24 +0900

    react first lesson

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDom from "react-dom";
 
-import App from "./App";
+const App = () => {
+  return null;
+};
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDom.render(<App />, document.getElementById("root"));
